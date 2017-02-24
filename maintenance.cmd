4dcb1f6063eeaea2bfbfa7c2fecdaa1112929422
prosmart <dna@edrs.com.au>
2017-02-24 16:32:04 +1000

    Fixed 2 x curl commands

--- maintenance.cmd
+++ maintenance.cmd
@@ -1,4 +1,4 @@
-@ECHO OFF 
+@ECHO OFF
 ::
 ::   Name: maintenance.cmd
 ::   Author: White Star Software
@@ -17,22 +17,17 @@ Setlocal EnableDelayedExpansion
 
 :: Grab environment vars
 
-if "%ENVPT3%"=="" (
-   echo "PROTOP environment variable not set - exiting maintenance.cmd"
+IF NOT EXIST "%PROTOP%\bin\protopenv.bat" (
+   echo Unable to locate %PROTOP%\bin\protopenv.bat
    GOTO :ABEND
 )
 
-IF NOT EXIST "%ENVPT3%\bin\protopenv.bat" (
-   echo Unable to locate %ENVPT3%\bin\protopenv.bat
-   GOTO :ABEND
-)
-
-CALL %ENVPT3%\bin\protopenv.bat
+CALL "%PROTOP%\bin\protopenv.bat"
 
 :: VARIABLES
 
-:: Unset this for normal use
-:: SET EKKO=echo
+:: Switch these around for normal use
+::SET EKKO=echo
 SET EKKO=
 
 SET AND=IF
@@ -45,7 +40,7 @@ SET "USAGE4=       maintenance.cmd status"
 
 ::   Process Parameters
 
-IF [%1] == [] (
+IF "%1" == "" (
    CALL :SUB_USAGE
    GOTO :ABEND
 )
@@ -97,60 +92,66 @@ SET /P CUSTID=<"%PROTOP%\etc\custid.cfg"
 
 :: Redirection at the beginning of the line avoids trailing space going to stdout
 
+
 IF "%ACTION%"=="on" (
 
-   >"%MFLAG%" echo %NOW% %DURATION%
-   >>"%MLOG%" echo %NOW% ON %DURATION% 
+   echo %NOW% %DURATION% > %MFLAG%
+   echo %NOW% ON %DURATION% >>%MLOG% 
 
    ECHO Commencing Maintenance Mode for %DURATION% hours
 
-   del /s %TMPDIR%\*.flg >NUL 2>&1
+   del /s "%TMPDIR%\*.flg" >NUL 2>&1
 
    set QSTR=sitename^=%CUSTID%^&dbIdList^=all^&description^=Created^+by^+maintenance.cmd
    
    set QSTR=!QSTR!^&duration=%DURATION%^&oper=add^&req=maintenance
 
-   >>"%MLOG%" echo curl -X POST --data !QSTR! %PTHOST%/cgi-bin/pt3admin.cgi 2>&1
-   >>"%MLOG%" %EKKO% curl -X POST --data !QSTR! %PTHOST%/cgi-bin/pt3admin.cgi 2>&1
+   >>%MLOG% echo curl -X POST --data "!QSTR!" %PTHOST%/cgi-bin/pt3admin.cgi 2>&1
+   >>%MLOG% %EKKO% curl -X POST --data "!QSTR!" %PTHOST%/cgi-bin/pt3admin.cgi 2>&1
 
+   GOTO END
 )
+
 
 IF "%ACTION%"=="off" (
 
-   IF NOT EXIST "%MFLAG%" (
+   IF NOT EXIST %MFLAG% (
       echo Maintenance mode is already off
       GOTO :END
    )
 
    ECHO Ending Maintenance Mode, restarting protop agents
 
-   %EKKO% DEL "%MFLAG%" >>%MLOG%
+   %EKKO% DEL %MFLAG% >>%MLOG%
 
 
-   IF EXIST "%MFLAG%" (
-      echo "Failed! Maintenance Mode is still on"
+   IF EXIST %MFLAG% (
+      echo Failed! Maintenance Mode is still on
       GOTO :ABEND
    )
 
-   >>"%MLOG%" echo %NOW% OFF
+   >>%MLOG% echo %NOW% OFF
 
    set QSTR=sitename=%CUSTID%^&oper=del^&req=maintenance
 
-   >>"%MLOG%" echo curl -X DELETE --data !QSTR! %PTHOST%/cgi-bin/pt3admin.cgi 2>&1
+   >>%MLOG% echo curl -X DELETE --data "!QSTR!" %PTHOST%/cgi-bin/pt3admin.cgi 2>&1
 
-   >>"%MLOG%" %EKKO% curl -X DELETE --data !QSTR! %PTHOST%/cgi-bin/pt3admin.cgi 2>&1
+   >>%MLOG% %EKKO% curl -X DELETE --data "!QSTR!" %PTHOST%/cgi-bin/pt3admin.cgi 2>&1
 
+   GOTO END
 )
+
 
 IF "%ACTION%"=="status" (
  
-   IF EXIST "%MFLAG%" (
-      SET /P TMP=<"%MFLAG%"
+   IF EXIST %MFLAG% (
+      SET /P TMP=<%MFLAG%
       echo Maintenance Mode has been on since !TMP!
       GOTO :END
    )
 
    echo Maintenance Mode is OFF
+   GOTO END
 )
 
 GOTO :END
